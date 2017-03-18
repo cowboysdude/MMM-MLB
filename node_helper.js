20e8d92b7e19596147b1b2de5680a14834984431
John Wade <cowboysdude@stny.rr.com>
2017-03-18 16:16:45 -0400

    Update for focus_on

--- node_helper.js
+++ node_helper.js
@@ -1,69 +1,76 @@
 /* Magic Mirror
- * Module: MMM-MLB
- *
- * By Cowboysdude
- * 
- */
+    * Module: MMM-MLB
+    *
+    * By Cowboysdude
+    * 
+    */
 const NodeHelper = require('node_helper');
 const request = require('request');
 const moment = require('moment');
 const fs = require('fs');
 
 module.exports = NodeHelper.create({
-
-    start: function() {
-        this.standings = {
+	
+	start: function() {
+		this.standings = {
             timestamp: null,
             data: null
         };
-        this.path = "modules/MMM-MLB/standings.json";
+       this.path = "modules/MMM-MLB/standings.json";
         if (fs.existsSync(this.path)) {
             var temp = JSON.parse(fs.readFileSync(this.path, 'utf8'));
             if (temp.timestamp === this.getDate()) {
                 this.standings = temp;
             }
         }
-        console.log("Starting module: " + this.name);
+    	console.log("Starting module: " + this.name);
     },
 
     getMLB: function(url) {
-        var nowYear = moment().format('YYYY');
+    	var nowYear = moment().format('YYYY'); 
         var nowMonth = moment().format('MM');
         var nowDay = moment().format('DD');
-
+    	
         request({
-            url: ("http://gd2.mlb.com/components/game/mlb/year_" + nowYear + "/month_" + nowMonth + "/day_" + nowDay + "/master_scoreboard.json"),
+            url: ("http://gd2.mlb.com/components/game/mlb/year_"+ nowYear +"/month_"+nowMonth+"/day_"+nowDay+"/master_scoreboard.json"),
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 var result = JSON.parse(body).data.games;
+                if(this.config.focus_on.length > 0){
+					result.game = result.game.filter((game) => {
+						if(this.config.focus_on.includes(game.home_team_name) || this.config.focus_on.includes(game.away_team_name)){
+							return true;
+						} else {
+							return false;
+						}
+					});
+				}
                 this.sendSocketNotification('MLB_RESULTS', result);
             }
         });
     },
-
-    GET_STANDINGS: function(url) {
-        request({
-            url: ("https://erikberg.com/mlb/standings.json"),
-            qs: {
-                from: 'https://forum.magicmirror.builders/',
-                time: +new Date()
-            },
-            method: 'GET',
-            headers: {
-                'User-Agent': 'MagicMirror'
-            }
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).standing;
-                this.sendSocketNotification('STANDINGS_RESULTS', result);
-                this.standings.timestamp = this.getDate();
-                this.standings.data = result;
-                this.fileWrite();
-            }
-        });
+    
+   GET_STANDINGS: function(url) {
+         request({
+             url: "https://erikberg.com/mlb/standings.json", 
+             method: 'GET', 
+             headers: {
+                 'User-Agent': 'MagicMirror/1.0 ('+this.config.email+')'
+             }
+         }, (error, response, body) => {
+             if (!error && response.statusCode == 200) {
+                 var result = JSON.parse(body).standing;
+                 this.sendSocketNotification('STANDINGS_RESULTS', result);
+                 this.standings.timestamp = this.getDate();
+                 this.standings.data = result;
+                 this.fileWrite();
+             }
+         });
     },
-
+    
+    // okay what did I do? I didn't change anything jst worked on the table'
+    //that all I added after it quit working because I was getting an outof data but it's not working because it's not firing anyway  sorry I have been out with the dog so I didnt see anything what exactly isnt working anymore can you still get the matches as you had in the beginning?  yes it's this not working'
     fileWrite: function() {
         fs.writeFile(this.path, JSON.stringify(this.standings), function(err) {
             if (err) {
@@ -72,16 +79,17 @@ module.exports = NodeHelper.create({
             console.log("The standings file was saved!");
         });
     },
-
+    
     getDate: function() {
         return (new Date()).toLocaleDateString();
     },
-
+    
     socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_MLB') {
-            this.getMLB(payload);
-        }
-        if (notification === 'GET_STANDINGS') {
+    	if(notification === 'CONFIG'){
+			this.config = payload;
+		} else if (notification === 'GET_MLB') {
+    		this.getMLB(payload);
+        } else if (notification === 'GET_STANDINGS') {
             this.GET_STANDINGS(payload);
         }
     }
